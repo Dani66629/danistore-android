acc91c83bb2d5ae3ae03a555a123f72a979654c5
Tu Nombre <tu_email@gmail.com>
2025-08-30 23:20:41 -0500

    Agregar DaniStore para Android

---
+++ main.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+Punto de entrada principal para DaniStore Android
+"""
+
+from danistore_app import DaniStoreApp
+
+if __name__ == '__main__':
+    DaniStoreApp().run()
